d08f3edf9e049f2ac2158268d9db0093d2dc4f85
eitanh <eitanh@como.com>
2019-02-02 07:30:30 +0200

    wip

--- lib/services/PdfTextExtractor.ts
+++ lib/services/PdfTextExtractor.ts
@@ -1,4 +1,5 @@
-import * as pdf from "pdf-parse";
+import * as pdf from "pdfjs-dist";
+
 import { ITestTextExtractor } from "./ITestTextExtractor";
 export class PdfTextExtractor implements ITestTextExtractor {
 
@@ -25,7 +26,8 @@ export class PdfTextExtractor implements ITestTextExtractor {
     public constructor(private fileStream: Buffer) { }
 
     public async extractQuestions(): Promise<string[]> {
-        const { text } = await pdf(this.fileStream);
+        const textWithMetadata = await extractPdfText(this.fileStream);
+        const text = textWithMetadata.toString();
         const tokens: RegExpExecArray[] = [];
         const exp = /\d+\s+מספר שאלה/g;
         let result = exp.exec(text);
@@ -40,4 +42,45 @@ export class PdfTextExtractor implements ITestTextExtractor {
         questions.push(text.substring(tokens[tokens.length - 1].index));
         return questions.map(PdfTextExtractor.fixRtlWordOrder);
     }
+}
+
+async function extractPdfText(dataBuffer) {
+    // Disable workers to avoid yet another cross-origin issue (workers need
+    // the URL of the script to be loaded, and dynamically loading a cross-origin
+    // script does not work).
+    pdf.disableWorker = true;
+    let doc = await pdf.getDocument(dataBuffer);
+    let ret = [];
+    for (var i = 1; i <= doc.numPages; i++) {
+        const pageData = await doc.getPage(i);
+        const pageText = await render_page(pageData);
+        ret.push(pageText);
+    }
+    doc.destroy();
+    return ret;
+}
+
+async function render_page(pageData) {
+    //check documents https://mozilla.github.io/pdf.js/
+    let render_options = {
+        //replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
+        normalizeWhitespace: true,
+        //do not attempt to combine same line TextItem's. The default value is `false`.
+        disableCombineTextItems: false
+    }
+    const textContent = await pageData.getTextContent(render_options);
+    let lastY, line = [];
+    const lines = [];
+    const itemTransform = (item, x, y) => ({text: item.str, x, y, page: pageData.pageIndex});
+    for (let item of textContent.items) {
+        const [,,,, x, y] = item.transform;
+        if (lastY == y || !lastY) {
+            line.push(itemTransform(item, x, y));
+        } else {
+            lines.push(line);
+            line = [itemTransform(item, x, y)];
+        }
+        lastY = y;
+    }
+    return lines;
 }
